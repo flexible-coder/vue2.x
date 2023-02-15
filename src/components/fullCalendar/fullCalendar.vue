4a586ce21d89746304f367590780dd8535687b94
kxm <57086044+flexible-coder@users.noreply.github.com>
2023-02-15 14:16:23 +0800

    feat: 日历组件调整

--- src/components/fullCalendar/fullCalendar.vue
+++ src/components/fullCalendar/fullCalendar.vue
@@ -1,5 +1,5 @@
 <template>
-  <FullCalendar ref="fullCalendar" :options="calendarOptions"></FullCalendar>
+  <FullCalendar ref="fullCalendar" :options="calendarOptions"> </FullCalendar>
 </template>
 
 <script>
@@ -17,25 +17,24 @@ export default {
       calendarApi: null,
       calendarOptions: {
         plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin], // 需要用哪个插件引入后放到这个数组里
-        initialDate: new Date(), // 日历第一次加载时显示的初始日期。可以解析为Date的任何职包括ISO8601日期字符串，例如"2014-02-01"。
         initialView: "dayGridMonth", // 日历加载时的初始视图，默认值为'dayGridMonth'，可以为任何可用视图的值，如例如'dayGridWeek'，'timeGridDay'，'listWeek'
         locale: "zh-cn", // 设置日历的语言，中文为 “zh-cn”
         firstDay: "1", // 设置每周的第一天，默认值取决于当前语言环境，该值为代表星期几的数字，且值必须为整数，星期日=0
         weekNumberCalculation: "ISO", // 指定"ISO"结果为ISO8601周数。指定"ISO"将firstDay的默认值更改为1（Monday）
-        unselectAuto: false, //当点击页⾯⽇历以外的位置时，是否⾃动取消当前的选中状态。false是不取消
+        // unselectAuto: false, //当点击页⾯⽇历以外的位置时，是否⾃动取消当前的选中状态。false是不取消
         selectable: true,
         buttonText: {
           // 文本将显示在headerToolbar / footerToolbar的按钮上。不支持HTML注入。所有特殊字符将被转义。
-          today: "今天",
-          month: "月",
+          today: "返回今天",
+          day: "日",
           week: "周",
-          day: "天",
+          month: "月",
         },
         headerToolbar: {
           // 在日历顶部定义按钮和标题。将headerToolbar选项设置为false不会显示任何标题工具栏。可以为对象提供属性start/center/end或left/center/right。这些属性包含带有逗号/空格分隔值的字符串。用逗号分隔的值将相邻显示。用空格分隔的值之间会显示一个很小的间隙。
           left: "prev today next",
           center: "title",
-          right: "dayGridMonth,dayGridWeek,dayGridDay",
+          right: "dayGridDay,dayGridWeek,dayGridMonth",
         },
         eventTimeFormat: {
           // 在每个事件上显示的时间的格式
@@ -43,32 +42,14 @@ export default {
           minute: "2-digit",
           hour12: false,
         },
-        // events: {
-        //   // 将在日历上显示的事件对象， events 可以是数组、json、函数。具体可以查看官方文档
-        //   title: "标题",
-        //   start: "事件开始事件",
-        //   end: "事件结束事件", // 这里要注意，end为可选参数，无end参数时该事件仅在当天展示
-        //   backgroundColor: "#FDEBC9", // 该事件的背景颜色
-        //   borderColor: "#FDEBC9", // 该事件的边框颜色
-        //   textColor: "#F9AE26", // 该事件的文字颜色
+        // customButtons: {
+        //   // 定义可在headerToolbar / footerToolbar中使用的自定义按钮
+        //   today: {
+        //     text: "返回今天", // 按钮的展示文本
+        //     click: this.todayClick, // 点击按钮触发的事件，这里要注意的是当按钮绑定了事件之后该按钮原本自带的事件将会失效
+        //   },
         // },
-        // events:[
-        //   { title: 'event 1', date: '2023-02-11' },
-        // ],
-        customButtons: {
-          // 定义可在headerToolbar / footerToolbar中使用的自定义按钮
-          today: {
-            text: "返回今天", // 按钮的展示文本
-            click: this.todayClick, // 点击按钮触发的事件，这里要注意的是当按钮绑定了事件之后该按钮原本自带的事件将会失效
-          },
-          // next: {
-          //   click: this.nextClick,
-          // },
-          // prev: {
-          //   click: this.prevClick,
-          // },
-        },
-        eventClick: this.handleDateClick, // 点击事件时，触发该回调
+        // eventClick: this.handleDateClick, // 点击事件时，触发该回调
         // eventMouseEnter: this.handleMouseEnter, // 鼠标悬停在事件上时，触发该回调
         // eventMouseLeave: this.handleMouseLeave, // 鼠标移除时，触发该回调
         dateClick: this.handleDateClick, // 当用户单击日期或时间时,触发该回调，触发此回调，您必须加载interaction插件
@@ -107,19 +88,16 @@ export default {
   },
   mounted() {
     this.calendarApi = this.$refs.fullCalendar.getApi();
-    //  this.calendarApi.gotoDate('2023-02-11');
-     console.log(this.calendarApi.getDate())
   },
   methods: {
     todayClick() {
       this.calendarApi.today(); // 将日历移动到当前日期。
       this.calendarApi.getDate(); // 返回日期的日历的当前日期
     },
-    handleDateClick(val) {
-      console.log(val);
-      const { dateStr } = val;
-      console.log(val.view.getCurrentData())
-      this.calendarApi.gotoDate(dateStr);
+    handleDateClick(obj) {
+      console.log('obj',obj)
+      // const { dateStr } = val;
+      // this.calendarApi.gotoDate(dateStr);
     },
     nextClick() {
       this.calendarApi.next(); // 将日历向前移动一步（例如，一个月或一周）。
@@ -138,14 +116,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-::v-deep {
+//月
+::v-deep .fc-dayGridMonth-view {
   .fc-daygrid-day-events {
     display: none; //注意-需要的话设置block
   }
   .fc-daygrid-day-top {
-    height: 75.6px;
     display: block;
     .dayCell {
+      //  height: 75.59px;
       width: 100%;
       height: 100%;
       display: flex;
@@ -160,9 +139,9 @@ export default {
         color: #f73131;
       }
     }
-    .festivalCell {
-      background: #fde3e4;
-    }
+    // .festivalCell {
+    //   background: #fde3e4;
+    // }
   }
 }
 </style>
